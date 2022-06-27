ccb75357357dc425b67459cb0fc07aff47576eec
lofbgmlve <lofbgmlve@naver.com>
2022-06-27 16:45:04 +0900

    tttttt mkdir Service

---
+++ test2Boot-2/src/main/java/com/test/test/testService.java
@@ -0,0 +1,15 @@
+package com.test.test;
+
+public class testService {
+
+    public testService() {
+    }
+    
+    public void testGit(){
+
+    }
+
+    public String goGit(){
+        return null;
+    }
+}
