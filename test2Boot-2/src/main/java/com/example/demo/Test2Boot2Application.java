fbafa586a228ab0b52daf9c7687515bc7a10b7b0
lofbgmlve <lofbgmlve@naver.com>
2022-06-27 15:33:36 +0900

    1

---
+++ test2Boot-2/src/main/java/com/example/demo/Test2Boot2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2Boot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test2Boot2Application.class, args);
+	}
+
+}
---
+++ test2Boot-2/src/main/java/com/test/test/testDao.java
@@ -0,0 +1,18 @@
+package com.test.test;
+
+public interface testDao {
+
+	public void testGit();
+	
+	public String newBranchTest2();
+	
+	public void testMerge();  //master - master
+	
+	public void testTest2();  //rebase
+	
+	//success
+	
+	//newBranch and master test success 
+	//and project git clone
+	
+}
