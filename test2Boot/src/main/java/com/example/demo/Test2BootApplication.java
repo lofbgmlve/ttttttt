2a5225cca15b27cb57272710f1c8fe0f132e176d
lofbgmlve <lofbgmlve@naver.com>
2022-06-27 16:57:23 +0900

    gitLab Test

---
+++ test2Boot/src/main/java/com/example/demo/Test2BootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test2BootApplication.class, args);
+	}
+
+}
