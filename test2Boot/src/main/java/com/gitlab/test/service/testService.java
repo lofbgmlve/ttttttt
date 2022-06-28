fc69764afd636b841989294536b166eeffa2c23a
lofbgmlve <lofbgmlve@naver.com>
2022-06-28 09:24:56 +0900

    service folder and file

---
+++ test2Boot/src/main/java/com/gitlab/test/service/testService.java
@@ -0,0 +1,11 @@
+package com.gitlab.test.service;
+
+public interface testService {
+    
+    
+	public String gitLab();
+	
+	public void testMaster(); // rebase and push
+
+	public void gitLabTest(); // main
+}
